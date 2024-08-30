3eaac4049ab88d0d7034eafd8edcc52037212c85
Alves <gfernandes@ges.inatel.br>
2024-08-30 18:59:57 -0300

    guys o maciel roda

---
+++ Assets/Shooting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Atira quando o botão esquerdo do mouse é pressionado
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
