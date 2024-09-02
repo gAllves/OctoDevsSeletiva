5c46c473b2f6af0730951cc5638969a6af638a1a
Alves <gfernandes@ges.inatel.br>
2024-09-01 22:33:07 -0300

    guys, nao pula mais a cerca

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Se a bala colidir com objeto com tag "Wall" ela é destruida
+        if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    
+}
--- Assets/Scripts/PlayerMoviment.cs
+++ Assets/Scripts/PlayerMoviment.cs
@@ -11,6 +11,7 @@ public class PlayerMoviment : MonoBehaviour
     public Vector2 movement;
     public Vector2 mousePos;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,18 +21,31 @@ public class PlayerMoviment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        //Movimentação do mouse
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-    }
 
-    void FixedUpdate()
-    {
+        //Movimentação do player
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
+        //Rotação do player
         Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
+
+        //Flip do player
+        if (mousePos.x > 0)
+        {
+            transform.localScale = new Vector3(0.1f,0.045f,1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.1f,-0.045f,1);
+        }
     }
+
+    
 }
