06d904c02e27547ba1b6bc796dbbc6bdb0f5b8fc
Alves <gfernandes@ges.inatel.br>
2024-09-16 19:50:48 -0300

    guys, temos firerate e arrumei as animacoes

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -18,20 +18,9 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(estaNaPorta == false)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(-6.0f, 0f), moveSpeed * Time.deltaTime);
-        }
-
-        if(transform.position.x == -6.0f)
-        {
-            estaNaPorta = true;
-        }
-
-        if (player != null && estaNaPorta)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
-        }
+ 
+    transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+        
     
     }
 
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -10,21 +10,36 @@ public class Shooting : MonoBehaviour
     public float bulletForce = 20f;
     public Animator oAnimator;
 
-    // Update is called once per frame
+    public float firerate = 0.4f;
+    public float canFire = -1f;
+
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") && Time.time > canFire)
         {
+            //chama a função de atirar
             Shoot();
+
+            //Ativa a animação de atirar
+            oAnimator.SetTrigger("isShooting");
+            oAnimator.ResetTrigger("isNotShooting");
+        }
+        else{
+
+            //Desativa a animação de atirar
+            oAnimator.SetTrigger("isNotShooting");
+            oAnimator.ResetTrigger("isShooting");
         }
     }
 
     void Shoot()
     {
+        //Cria a bala
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
 
-        oAnimator.SetTrigger("isShooting");
+        canFire = Time.time + firerate;
+
     }
 }
